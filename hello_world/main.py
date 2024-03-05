e8adcc965150cfc244e667d832af39c2e19f1499
rceglie <ceglierob@gmail.com>
2024-03-05 14:43:21 -0500

    cleaned up endpoint

--- hello_world/main.py
+++ hello_world/main.py
@@ -1,36 +1,98 @@
-# Copyright 2015 Google LLC
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
 # [START gae_flex_quickstart]
-from flask import Flask
-
+from flask import Flask, make_response
+from pybaseball import batting_stats, pitching_stats
+import pandas as pd
 
 app = Flask(__name__)
 
+battingCategories = [
+    "Name",
+    "Team",
+    "AVG",
+    "OBP",
+    "SLG",
+    "OPS",
+    "BABIP",
+    "wRC+",
+    "xBA",
+    "wOBA",
+    "xwOBA",
+]
+pitchingCategories = [
+    "Name",
+    "Team",
+    "ERA",
+    "ERA-",
+    "FIP",
+    "xFIP",
+    "FIP-",
+    "xFIP-",
+    "WHIP",
+    "SIERA",
+]
+teamCorrections = {
+    "ARI": "Ari",
+    "ATL": "Atl",
+    "BAL": "Bal",
+    "BOS": "Bos",
+    "CHC": "ChC",
+    "CWH": "ChW",
+    "CIN": "Cin",
+    "CLE": "Cle",
+    "COL": "Col",
+    "DET": "Det",
+    "HOU": "Hou",
+    "KCR": "KC",
+    "LAA": "LAA",
+    "LAD": "LAD",
+    "MIA": "Mia",
+    "MIL": "Mil",
+    "MIN": "Min",
+    "NYY": "NYY",
+    "NYM": "NYM",
+    "OAK": "Oak",
+    "PHI": "Phi",
+    "PIT": "Pit",
+    "SDP": "SD",
+    "SFG": "SF",
+    "SEA": "Sea",
+    "STL": "StL",
+    "TBR": "TB",
+    "TEX": "Tex",
+    "TOR": "Tor",
+    "WSH": "Wsh",
+}
+nameCorrections = {
+    "Luis Robert": "Luis Robert Jr.",
+    "Cedric Mullins II": "Cedric Mullins",
+    "T.J. Friedl": "TJ Friedl",
+}
+
 
 @app.route("/")
-def hello() -> str:
-    """Return a friendly HTTP greeting.
+def hello():
+    bdf = batting_stats(2023, qual=1)
+    bdf = bdf[battingCategories]
 
-    Returns:
-        A string with the words 'Hello World!'.
-    """
-    return "Hello World!"
+    pdf = pitching_stats(2023, qual=1)
+    pdf = pdf[pitchingCategories]
+
+    df = pd.concat([bdf, pdf], ignore_index=True)
+
+    df["Team"] = df["Team"].replace(teamCorrections)
+    df["Name"] = df["Name"].replace(nameCorrections)
+
+    agg = {col: "sum" if col not in ["Name", "Team"] else "first" for col in df.columns}
+    df = df.groupby(["Name", "Team"]).aggregate(agg)
+
+    df.to_csv("stats.csv", index=False)
+
+    response = make_response(df.to_csv(index=False))
+    response.headers["Content-Disposition"] = "attachment; filename=data.csv"
+    response.headers["Content-Type"] = "text/csv"
+
+    return response
 
 
 if __name__ == "__main__":
-    # This is used when running locally only. When deploying to Google App
-    # Engine, a webserver process such as Gunicorn will serve the app.
     app.run(host="127.0.0.1", port=8080, debug=True)
-# [END gae_flex_quickstart]
